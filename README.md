bcb395a80e2c402c29582b3ae2f7489259a23a7e
Humberto Corredor <corredor28@gmail.com>
2023-08-09 19:00:18 -0500

    Update instructions and scripts

--- README.md
+++ README.md
@@ -2,10 +2,10 @@
 
 INSTRUCTIONS:
 
-1. Deploy d_database project to local database (you can use the included publish profile d_database.publish.xml)
-2. (Optional) Run the Scripts\04_insert_5000_animals.sql script of the d_database project to populate data
-3. Run the API (q_api project)
-4. Run the POST /token method, using one of the test users from appsettings.json as the body *
-5. Copy token from result and use it to call other methods
+1. Deploy d_database project to local database (you can use the included publish profile d_database.publish.xml).
+2. (Optional) Run the Scripts\04_insert_5000_animals.sql script of the d_database project to populate data.
+3. Run the API (q_api project).
+4. Run the POST /token method, using one of the test users from appsettings.json as the body. *
+5. Copy token from result and use it to call other methods.
 
-* An example Postman collection is provided on the git root folder
+* An example Postman collection is provided on the git root folder. This collection has the call to token endpoint scripted so it is easier to test.
--- farm_api/d_database/Scripts/07_extras.sql
+++ farm_api/d_database/Scripts/07_extras.sql
@@ -12,19 +12,20 @@ BEGIN
 END;
 GO
 
-IF NOT EXISTS (SELECT * FROM sysobjects where name='OrderItem' and xtype='U')
-BEGIN
-    CREATE TABLE [dbo].[OrderItem] (
-        [OrderItemId]	INT NOT NULL PRIMARY KEY IDENTITY,
-        [OrderId]		INT NOT NULL,
-        [AnimalId]		INT NOT NULL,
-        [Quantity]		INT NOT NULL,
-        [UnitPrice]		DECIMAL (18, 2) NOT NULL,	-- save purchase value in case it changes afterwards
-		[Discount]		DECIMAL (18, 2) NOT NULL,
-		[Price]			DECIMAL (18, 2) NOT NULL,
-		FOREIGN KEY ([OrderId]) REFERENCES [Order]([OrderId]),
-		FOREIGN KEY ([AnimalId]) REFERENCES [Animal]([AnimalId]),
-    );
-END;
-GO
+
+--IF NOT EXISTS (SELECT * FROM sysobjects where name='OrderItem' and xtype='U')
+--BEGIN
+--    CREATE TABLE [dbo].[OrderItem] (
+--        [OrderItemId]	INT NOT NULL PRIMARY KEY IDENTITY,
+--        [OrderId]		INT NOT NULL,
+--        [AnimalId]		INT NOT NULL,
+--        [Quantity]		INT NOT NULL,
+--        [UnitPrice]		DECIMAL (18, 2) NOT NULL,	-- save purchase value in case it changes afterwards
+--		[Discount]		DECIMAL (18, 2) NOT NULL,
+--		[Price]			DECIMAL (18, 2) NOT NULL,
+--		FOREIGN KEY ([OrderId]) REFERENCES [Order]([OrderId]),
+--		FOREIGN KEY ([AnimalId]) REFERENCES [Animal]([AnimalId]),
+--    );
+--END;
+--GO
 
